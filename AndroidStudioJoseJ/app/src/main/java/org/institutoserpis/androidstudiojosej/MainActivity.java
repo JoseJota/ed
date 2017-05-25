b823c8c2fa61e49a3a40a35b8a02cd3ab1cba1d6
JoseJota <jose.julian.perez.lucas@gmail.com>
2017-05-25 10:46:51 +0200

    Ejercicio final AndroidStudio

---
+++ AndroidStudioJoseJ/app/src/main/java/org/institutoserpis/androidstudiojosej/MainActivity.java
@@ -0,0 +1,73 @@
+package org.institutoserpis.androidstudiojosej;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView txtRec = (TextView)findViewById(R.id.txtRecTemp);
+        final TextView txtLibra = (TextView)findViewById(R.id.txtFarenheit);
+        final TextView txtKilo = (TextView)findViewById(R.id.txtKelvin);
+
+        final SeekBar seekBar1 = (SeekBar)findViewById(R.id.seekBar);
+
+        seekBar1.setMax(3000);
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                txtRec =(double) (progress/10.0);
+
+                String texto = formatNumber (progress/10.0);
+                texto +=" ºC";
+                txtRec.setText(texto);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        final Button btnConverter = (Button)findViewById(R.id.btnConverter);
+btnConverter.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+        double Farenheit = (((txtRec)*9/5+32));
+        String texto = formatNumber(Farenheit);
+        texto += "ºF";
+        R.id.txtFarenheit.setText(texto);
+
+        double Kelvin =(txtRec + 273);
+        String texto2 = formatNumber(Kelvin);
+        texto2 += "K";
+        R.id.txtKelvin.setText(texto2);
+    }
+});
+        });
+
+
+    }
+    private String formatNumber(double valor){
+        return String.format(Locale.FRANCE,"0.2f", valor);
+    }
+
+}
